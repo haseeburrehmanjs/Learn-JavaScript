4c4d984eaee45983e771c33030aca5e9b264e882
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-02-23 10:36:31 +0500

    modified work

--- JavaScript-01/app.js
+++ JavaScript-01/app.js
@@ -1,8 +1,38 @@
-alert ("what is your Name")
-alert ("Haseeb")
+// alert ("Error! Please enter a valid password")
+// alert ("Wellcome to Js land\ Happy Coding")
 
-var userName = "Haseeb"
-var lastName = "Ur Rehman"
-var age = 20
 
-console.log("My Name Is " + userName + " " + lastName + " and my age is" + " " + age);
+
+// var userName = "Haseeb"
+// var lastName = "Ur Rehman"
+// var age = 20
+
+// console.log("My Name Is " + userName + " " + lastName + " and my age is" + " " + age);
+
+
+// var num1 = +prompt('Enter First Num')
+// var num2 = +prompt('Enter First Num')
+// var num3 = +prompt('Enter First Num')
+// var num4 = +prompt('Enter First Num')
+
+// console.log(num1 + num2 + num3 + num4);
+
+
+// var num1 = +prompt("enter your name")
+// var num2 = +prompt("enter your last name")
+
+// console.log(num1 + num2)
+
+// var num1 = +prompt("enter first num");
+// var num2 = +prompt("enter second num");
+
+// console.log(num1 >= num2);
+
+
+// var num1 = prompt('enter first num');
+// var num2 = prompt('enter sec num');
+
+// console.log(num1 !== num2);
+// console.log(num1 != num2);
+
+// console.log(10  '10');
