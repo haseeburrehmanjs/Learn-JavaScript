6f99341c5ed4945257b74d0191d364d80b8c98e7
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-02-24 15:37:29 +0500

    modified work

---
+++ JavaScript-Chapter/app.js
@@ -0,0 +1,55 @@
+// ==== Chapter 01 ====
+
+
+// ==== Chapter 01 Q 1 ====
+// alert('Error! Please enter a valid password.')
+
+// ==== Chapter 01 Q 2 ====
+// alert('Wellcome to JS land\nHappy Coding.')
+
+// ==== Chapter 01 Q 3 ====
+// alert('Welcome to js Land...')
+
+// ==== Chapter 01 Q 4 ====
+// var message = 'Hello... I can run JS through my werb browers console.';
+// alert(message)
+
+
+// ==== Chapter 02 ====
+
+
+// ==== Chapter 02 Q 1 2 3 4  ====
+// var userName = 'Haseeb Ur Rehman'
+// var age = '16 years old';
+// var info = 'Certified Web and app development';
+
+// alert(userName);
+// alert(age);
+// alert(info);
+
+// ==== Chapter 02 Q 5  ====
+// var food = 'PIZZA\nPIZZ\nPIZ\nPI\nP'
+// alert(food)
+
+// ==== Chapter 02 Q 6  ====
+// var email = 'My email address is haseeb@gmail.com'
+// alert(email)
+
+// ==== Chapter 02 Q 7  ====
+// var book = 'I am trying to leran from the book A smarter\nway to learn javascript'
+// alert(book)
+
+// ==== Chapter 02 Q 8  ====
+// var htmlContent = 'Hye! I can write Html content Throught JavaScipt'
+// alert(htmlContent)
+
+// ==== Chapter 02 Q 9  ====
+// var jsBoder = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(jsBoder)
+
+
+// ==== Chapter 03 ====
+
+
+// ==== Chapter 03  Q 1====
+
