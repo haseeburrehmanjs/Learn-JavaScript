430a8e3f5e23217410d607a5a03a43f1c9d61e4a
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-02-24 14:00:05 +0500

    modified work

---
+++ app.js
@@ -0,0 +1,22 @@
+// ==== Chapter 01 ====
+
+// ==== Chapter 01 Q 1 ====
+// alert('Error! Please enter a valid password.')
+
+// ==== Chapter 01 Q 2 ====
+// alert('Wellcome to JS land\nHappy Coding.')
+
+// ==== Chapter 01 Q 3 ====
+// alert('Welcome to js Land...')
+
+// ==== Chapter 01 Q 4 ====
+// var message = 'Hello... I can run JS through my werb browers console.';
+// alert(message)
+
+
+// ==== Chapter 02 ====
+
+// ==== Chapter 02 Q 1  ====
+
+
+
