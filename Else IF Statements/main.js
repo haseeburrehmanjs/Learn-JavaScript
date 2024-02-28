241df16ce0f70f13047c0577a3594aacf2f3a7fc
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-02-28 20:21:42 +0500

    modified work

--- Else IF Statements/main.js
+++ Else IF Statements/main.js
@@ -1,13 +1,53 @@
 // === IF ELSE STATEMENTS ===
 
-// Write a program to check whether the given input number is
+
+// === IF ELSE STATEMENTS Q 1 ===
+
+// 1. Write a program to check whether the given input number is
 // divisible by 3 or else show a message “Number is not divisible
 // by 3”.
+ 
+// var number = +prompt('Write Your Number Here!')
 
-var number = +prompt('Write Your Number Here!')
+// if (number % 3 === 0){
+//     console.log('Number is divion by 3.');
+// }else {
+//     console.log("Number is not divion by 3");
+// }
 
-if (number % 3 === 0){
-    console.log('Number is divion by 3.');
-}else {
-    console.log("Number is not divion by 3");
-}
+// === IF ELSE STATEMENTS Q 2 ===
+
+// 2. Write a program that checks whether the given input is an even
+// number or an odd number.
+
+// var evenOdd = +prompt('Write Number Here!')
+
+// if (evenOdd % 2){
+//     console.log('this is odd Number')
+// }else {
+//     console.log('this is even number');
+// }
+
+
+// === IF ELSE STATEMENTS Q 3 ===
+
+// 3. Write an if/else statement with the following condition:
+// If the variable age is greater than 18, output "Old enough",
+// otherwise output "Too young".
+
+// var age = +prompt('Write your age here!')
+
+// if (age <= 18){
+//     console.log('Too young');
+// }else {
+//     console.log('Too enough');
+// }
+
+
+// === IF ELSE STATEMENTS Q 4 ===
+
+
+
+
+
+
