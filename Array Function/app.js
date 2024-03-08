e3bd28ea7e8bd2d522bd61c298cda4dd7168f36a
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-08 21:23:34 +0500

    modified work

---
+++ Array Function/app.js
@@ -0,0 +1,30 @@
+// JavaScript Array Function
+
+//push {Last ma value add krna ka liya.}
+//pop {Last wali value remove krna ka liya}
+//unshift {start ma value add krna ka liya.}
+//shift {start ma value remove krna ka liya}
+//splice {Bich ma sa value hata na ka liya}
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+let softwere = ['Illustratal', 'PhotoShop', 'Figma', 'Indesign']
+// softwere.push('premiere') {last ma value add krna ka liya}
+// softwere.pop('')  {last ma value reomve krna ka liya}
+// softwere.unshift('Vs code') {start ma value add krna ka liya}
+// softwere.shift('') { start ma value add krna ka liya}
+// softwere.splice(1 , 1) {bich ma sa value hata or laga na ka liya}
+// softwere.length=0 {Array ki length 0 krna ka liya}
+// softwere.splice(1 , 2)
+console.log(softwere);
+
+// .length = Array ki length Check krna ka liya 
+// console.log(softwere.length);
+
+// console.log(softwere[1]);
