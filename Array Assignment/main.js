ecd02ebd6bea5decc154db7acfaded41e0bcf99d
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-12 00:21:53 +0500

    modified work

---
+++ Array Assignment/main.js
@@ -0,0 +1,41 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// let futureStudents = new Array();
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// let futureStudent = [];
+
+// 3. Declare and initialize a strings array
+
+// let studenName = ['Haseeb'];
+
+// 4. Declare and initialize a numbers array.
+
+// let favNumber = [10, 9, 8, 7, 6,];
+// console.log(favNumber);
+
+// 5. Declare and initialize a boolean array.
+
+// let boolean = [true, false]
+// console.log(boolean);
+
+// 6. Declare and initialize a mixed array.
+
+// let mixedArray = ['string', 9 , true, false];
+
+// 7. Declare and Initialize an array and store available mobile
+// networks in Pakistan.
+
+// let Array = [`available mobile networks in Pakistan.`]
+
+// 8. Declare and Initialize an array and store available education
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser
+// like:
+
+let qualifications = ['EG', 'SSC', 'HSC', 'BSC', 'BS', 'BCOM',] 
+console.log(qualifications);
+
