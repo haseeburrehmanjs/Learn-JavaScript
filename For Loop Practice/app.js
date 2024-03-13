fbc73a2d49098d2d7baa7ca325fe45dbb974b4ab
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-13 20:08:38 +0500

    modified work

--- For Loop Practice/app.js
+++ For Loop Practice/app.js
@@ -24,4 +24,54 @@ let array = ['apple', 'banana', 'grapes', 'mashmelo'];
 //     ul.innerHTML += `<li>${array[i]}</li>`
 // }
 
-let ul = document.
+// for (let i = 1; 1 <= 5 ; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+// console.log('done');
+
+
+// for (let i = 1; i <= 100; i++) {
+//     // console.log(i);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// let userValue = +prompt('Enter Table Number');
+// let times = +prompt('How many time do you want?')
+
+// for (let userValue = times; userValue <= times; userValue++) {
+//     console.log(userValue);
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${userValue} x ${j} = ${j*userValue}`);
+//     }
+// }
+
+// let userTable = +prompt('Enter Table number')
+// let times = +prompt('How many times do you want?')
+
+// for(let userTable = times; userTable <= times; userTable++ ){
+//     console.log(`${userTable} ka Table`);
+//     for(let j = 1; j <= times; j++){
+//         console.log(`${userTable} x ${j} = ${userTable*j}`);
+//     }
+// }
+
+// let userTable = +prompt('Enter Table number')
+// let times = +prompt('How many times do you want?')
+
+
+// for(let userTable = times; userTable <= times; userTable++){
+//     console.log(userTable);
+//     for(let j = 1; j <= times; j++){
+//         console.log(`${userTable} x ${j} = ${userTable*j}`);
+//     }
+// }
+
+
+
+
