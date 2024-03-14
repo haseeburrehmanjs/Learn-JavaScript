05a1a195d5347ebfe35f75ac84a336c47bf6419b
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-14 17:58:10 +0500

    ,odified wokr

--- Array Assignment/main.js
+++ Array Assignment/main.js
@@ -29,16 +29,50 @@
 // 7. Declare and Initialize an array and store available mobile
 // networks in Pakistan.
 
-// let Array = [`available mobile networks in Pakistan.`]
+// let AvailableNetwork = ["Zong", "Ufone", "Jazz", "Telenor"];
+// console.log(AvailableNetwork);
 
 // 8. Declare and Initialize an array and store available education
 // qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
 // M. Phil., PhD). Show the listed qualifications in your browser
 // like:
 
-let qualifications = ['EG', 'SSC', 'HSC', 'BSC', 'BS', 'BCOM',] 
+// let qualifications = ['EG', 'SSC', 'HSC', 'BSC', 'BS', 'BCOM',] 
+// let ul = document.querySelector('ul')
 
-let li = document.querySelector('li')
+// for(let i = 1; i <= qualifications.length; i++){
+//     ul.innerHTML += `<li>${qualifications[i]}</li>`
+// }
+
+// 9. Declare and initialize an empty array to store top movies of
+// 2015. Add movies one by one in an array. Display the elements
+// & length of array in your browser. (Use array’s length method)
+
+//  let array = ["Avengers' age of Ultern", "Spectr", "Inside Out", "Jurrasic World"]
+//  console.log(array);
+//  console.log(array.length);
+
+// 10. Declare and Initialize an array with your favorite cars. Show
+// a. First index of the array
+// b. Car at first index of the array
+// c. Last index of the array
+
+// let favorite_cars = ["Tesla Model S", "Porsche 911", "Lamborghini Aventador", "BMW M5"]
+
+// console.log(`This First Index ${favorite_cars[0]}`);
+// console.log(`This First Index ${favorite_cars[1]}`);
+// console.log(`This First Index ${favorite_cars[2]}`);
+// console.log(`This First Index ${favorite_cars[4]}`);
+// console.log(`This First Index ${favorite_cars[5]}`);
+
+// 11. Write a program to store 3 student names in an array. Take
+// another array to store score of these three students. Assume
+// that total marks are 500 for each student, display the scores &
+// percentages of students like:
+
+let studentName = ['Haseeb Marks is 320. Percentage is 78%', 'Haseeb Marks is 320. Percentage is 78%','Haseeb Marks is 320. Percentage is 78%']
+
+console.log(studentName);
 
 
 
