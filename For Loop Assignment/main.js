b52e0d844650fe42bcd6893502cbe6f7f9372f45
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-23 15:11:09 +0500

    modified wokr

--- For Loop Assignment/main.js
+++ For Loop Assignment/main.js
@@ -2,9 +2,12 @@
 // in your browser using for loop.
 
 // let message = "Hello World"
-// for(let i = 1; i <= 5; i++)[
-//     console.log(message);
-// ]
+// let ul = document.querySelector('ul')
+
+// for(let i = 1; i <= 5; i++){
+//     ul.innerHTML += `<li>${message} ${i}</li>`
+// }
+    
 
 // 2. Write a program to print numeric counting from 1 to 10.
 
@@ -39,11 +42,34 @@
 // 5.Write a program to print items of the following array using for
 // loop:
 
-fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+// fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
 
-let ul = document.querySelector('#orderlist');
-let para = document.querySelector('#orderlist')
+// let ul = document.querySelector('#orderlist');
+// let para = document.querySelector('#orderlist')
 
-for (let i = 0; i < fruits.length; i++) {
-    ul.innerHTML += `<li>Element at index ${i} ${fruits[i]}</li>`
-}
+// for (let i = 0; i < fruits.length; i++) {
+//     ul.innerHTML += `<li>Element at index ${i} ${fruits[i]}</li>`
+// }
+
+// 6. Write a program to initialize an array of N items by using
+// prompt. Where N is number of items as entered by the user.
+
+// let head = document.querySelector(".head");
+// let paragraph = document.querySelector(".para");
+// // let uservalue = +prompt('Enter Your Number');
+// // head.innerHTML = `Enter Your ${uservalue}`
+// let items = prompt('Enter Item Your Number')
+
+// for(let i = 0; i < items; i++){
+//     paragraph.innerHTML = `<li>Your Item Number ${items}</li>`
+// }
+
+// not complete xxxxxxxxxxxxxxxxxxxxxx
+
+// 7. Generate the following series in your browser. See example
+// output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
