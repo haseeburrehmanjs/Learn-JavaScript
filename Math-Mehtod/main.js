e81f0171281afc84d47e5267fcc41c0e857bd8b0
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-23 10:37:03 +0500

    modified work

---
+++ Math-Mehtod/main.js
@@ -0,0 +1,34 @@
+// Round 
+// Matlab ka desimal value .5 sa bari ho to 3 ha to 4 ho jai ga agr.5 sa choti to 3 hi raha ga!
+
+// ceil
+// Higest Value print kara na ka liya like 2.1 bhi hoto wo 3 print hi krai ga matlab ka desimal value ko bhi wo 1 smja ga!
+
+// floor
+// 
+// random
+// parseInt
+// ParseFloat
+
+// let num = 2.8;
+// console.log(Math.round(num));
+
+// let num = 2.5; 
+// console.log(Math.ceil(num));
+
+let x = Math.round(5.7)
+console.log(x);
+
+
+// let h1 = document.querySelector('h1');
+// let num = 1;
+
+
+// function toss(clickBtn){
+//     console.log("TOSS" , clickBtn);
+//     console.log(Math.ceil(Math.random(num)*2));
+//     if(num == 2){
+//         h1.innerHTML += "TAIL <br />"
+//     }else {
+//         h1.innerHTML += 'Head <br />'
+//     }
