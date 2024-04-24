6d71544776cf936addb47bb1916a9c565dc5e9f5
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-24 22:18:29 +0500

    modified work

--- Math-Mehtod/main.js
+++ Math-Mehtod/main.js
@@ -5,23 +5,43 @@
 // Higest Value print kara na ka liya like 2.1 bhi hoto wo 3 print hi krai ga matlab ka desimal value ko bhi wo 1 smja ga!
 
 // floor
-// 
+// Point ka bad wali value ko .5 ko zero hi smja ga!
+
+// trunc 
+// Point ka bad wali value na show nahi krta!
+
+// pow 
+// power check krna ka liya!
+
+// sqrt
+// squre pata krna ka liya!
+
 // random
+// random number lana ka liya 
+
 // parseInt
 // ParseFloat
 
 // let num = 2.8;
-// console.log(Math.round(num));
+// console.log(Math.round(num)); ==> point ka bad wali value agr .5 sa bari ho to 2. hoto 3 print krai ga agr .5 sa choti hoto 2.4 ha to 2 hi print krain ga
 
 // let num = 2.5; 
-// console.log(Math.ceil(num));
+// console.log(Math.ceil(num)); 
 
-let x = Math.round(5.7)
-console.log(x);
+// let x = Math.ceil(Math.round(5.7))==> .5 ka bad wali value ko 1 smja ga!
+// console.log(x);
 
+// let x = Math.floor(5.6)  // ==> point k bad wali value ko 0 hi smja ga 
+// console.log(x);
 
 // let h1 = document.querySelector('h1');
 // let num = 1;
+
+// let x = Math.pow(10, 3) // ==> Power nikal na ka liya!
+// console.log(x);
+
+// let x = Math.sqrt(100); // Squre nikal na ka liya!
+// console.log(x);
 
 
 // function toss(clickBtn){
