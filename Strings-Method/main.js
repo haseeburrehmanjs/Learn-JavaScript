d7709b7ae821fa0be65f5d27c56152a306e721d9
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-25 15:27:39 +0500

    String Method

---
+++ Strings-Method/main.js
@@ -0,0 +1,52 @@
+// toLowerCase
+// toUpperCase
+// length
+// charAt
+// indexOf
+// slice
+// replace
+// replaceall
+// split
+
+
+// ====== STRINGS METHOD =====
+
+// 1. Write a program that takes two user inputs for first and last
+// name using prompt and merge them in a new variable titled
+// fullName. Greet the user using his full name
+
+// let fName = prompt('Enter first Name!')
+// let lName = prompt('Enter last Name!')
+// let fullName = fName + lName
+// console.log(fullName);
+
+// =====> COMPLETEED TASK!
+
+// 2. Write a program to take a user input about his favorite mobile
+// phone model. Find and display the length of user input in your
+// browser.
+
+// let userName = 'Haseeb Ur Rehman'
+// let userLenght = userName.length
+// console.log(userLenght);
+
+// =====> COMPLETEED TASK!
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser.
+
+// let countryName = 'Pakistani'
+// let check = countryName.indexOf('n')
+// console.log(check);
+
+// =====> COMPLETEED TASK!
+
+// 4. Write a program to find the last index of letter “l” in the word
+// “Hello World” and display the result in your browser.
+
+let user = 'Hello World'
+console.log(user.length);
+console.log(user.indexOf(11));
+
+
+
