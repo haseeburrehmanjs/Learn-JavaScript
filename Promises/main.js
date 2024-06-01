cd894fef66bb66fa03418daa2d32bea948f6caef
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-01 10:13:09 +0500

    modified work

--- Promises/main.js
+++ Promises/main.js
@@ -42,7 +42,7 @@ let div = document.querySelector('div')
 axios('https://fakestoreapi.com/products')
 .then((res)=>{
     console.log(res.data);
-    res.data.map(item => {
+    res.data.map((item,index) => {
         div.innerHTML += `
         <div class="card p-3" style="width: 18rem;">
   <img src="${item.image}" width='300' height='300' class="card-img-top" alt="...">
@@ -50,7 +50,7 @@ axios('https://fakestoreapi.com/products')
     <h5 class="card-title">Price : $${item.price}</h5>
     <h5 class="card-title">${item.category}</h5>
     <p class="card-text">Rating : ${item.rating.rate} Count : ${item.rating.count}</p>
-    <a href="#" class="btn btn-danger">Add to Card</a>
+    <a class="btn btn-info" onclick='addtocard(${index})'>Add to Card</a>
   </div>
 </div>
         `
@@ -63,3 +63,67 @@ axios('https://fakestoreapi.com/products')
 
 
 
+// axios('https://fakestoreapi.com/users')
+// .then((responce)=>{
+//   console.log(responce.data);
+//   responce.data.map(item => {
+//     // console.log(item);
+//     div.innerHTML += `
+//   <h1>${item.name.firstname} ${item.name.lastname}</h1>
+//   <h1>${item.id}</h1>
+//   <h1>${item.address}</h1>
+//   <h1></h1>
+//   <hr />
+//   `
+//   })
+// })
+// .catch(error => {
+//   console.log(error);
+// })
+
+
+
+const againCard = document.querySelector('.again-Card')
+
+axios('https://fakestoreapi.com/products')
+.then(responnce => {
+  console.log(responnce.data)
+  responnce.data.map(item => {
+    console.log(item);
+    againCard.innerHTML += `
+  <div class="card" style="width: 18rem;" p-3>
+  <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+  `
+  })
+})
+.catch(error => {
+  console.log(error);
+})
+
+
+// let modal = document.querySelector('.modal1')
+// function addtocard(i){
+//   console.warn(
+//   <div class="modal" tabindex="-1">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <h5 class="modal-title">Modal title</h5>
+//         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//       </div>
+//       <div class="modal-body">
+//         <p>Modal body text goes here.</p>
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//   )
+// }
