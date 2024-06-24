d4b6ba6dc0bfee3287ca6b6889d5aeb69e169c5f
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-24 23:53:02 +0500

    modified work

--- Modules/about.js
+++ Modules/about.js
@@ -1,3 +1,8 @@
-import hello  from "./main.js";
+// import hello  from "./main.js";
 
-console.log(hello('Abdullah'));
+// console.log(hello('Abdullah'));
+
+import {add, sum} from './main.js'
+
+console.log(add(10,40));
+console.log(sum(30,10));
--- Modules/main.js
+++ Modules/main.js
@@ -1,7 +1,17 @@
-function hello(username){
-    return username
+// function hello(username){
+//     return username
+// }
+
+// console.log(hello('haseeb'));
+
+// export default hello
+
+export function add(number1, number2){
+    return number1 + number2
 }
+console.log(add(10,50));
 
-console.log(hello('haseeb'));
-
-export default hello
+export function sum(number1, number2){
+    return number1 - number2
+}
+console.log(sum(50,10));
